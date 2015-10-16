d7282e95da2957659f190ea11695fe9b0104a937
Aashis Khanal <aashis.khanal@tektak.com>
2015-10-16 10:53:42 +0545

    all

--- src/main/java/patterns/decorator/Game.java
+++
@@ -1,8 +0,0 @@
-package patterns.decorator;
-
-/**
- * Created by Aashis Khanal on 8/18/15.
- */
-public interface Game {
-    public void play();
-}
--- src/main/java/patterns/decorator/singleton/Living.java
+++
@@ -1,8 +0,0 @@
-package patterns.decorator.singleton;
-
-/**
- * Created by Aashis Khanal on 8/19/15.
- */
-public interface Living {
-    public void printName();
-}
